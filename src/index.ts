2e85a781c11436b3f44902f92a07fb44602ddfa3
Daniel Mitrov <me@danielmitrov.com>
2020-10-04 14:15:52 +0300

    Added simple express server

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 5001;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(cookieParser())
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello ${req.username}!`);
+});
+
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`);
+});
+
+export default {
+    app,
+    port,
+};
