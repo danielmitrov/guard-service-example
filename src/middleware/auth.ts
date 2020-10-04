8c324f07ddd8242e529ad89796854713510ce01a
Daniel Mitrov <me@danielmitrov.com>
2020-10-04 14:29:33 +0300

    Verify Guard's token

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,10 +1,26 @@
 import {Request, Response, NextFunction} from 'express';
 
 import {authBaseAPI, appName} from '../consts';
+import {validateGuardToken} from '../utils/validateGuardToken';
+import {createToken} from '../utils/serviceToken';
 
 
 function getCurrentUrl(req) {
     return req.protocol + '://' + req.get('host') + req.originalUrl;
+}
+
+async function handleGuardToken(req: Request, res: Response) {
+    try {
+        const payload = await validateGuardToken(req.query.token);
+
+        const redirectUrl = new URL(getCurrentUrl(req));
+        redirectUrl.searchParams.delete('token');
+
+        res.cookie('token', createToken(payload));
+        res.redirect(redirectUrl.href);
+    } catch(e) {
+        return res.status(401).send({error: 'invalid token'});
+    }
 }
 
 async function handleUnauthenticated(req: Request, res: Response) {
@@ -17,7 +33,11 @@ async function handleUnauthenticated(req: Request, res: Response) {
 }
 
 async function authenticate(req: Request, res: Response, next: NextFunction) {
-    handleUnauthenticated(req, res);
+    if (req.query.token) {
+        handleGuardToken(req, res);
+    } else {
+        handleUnauthenticated(req, res);
+    }
 }
 
 export default authenticate;
